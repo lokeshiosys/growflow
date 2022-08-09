b5f2ea6a3d1cb200ab6bd8367d064d70815463d5
lokeshiosys <110451803+lokeshiosys@users.noreply.github.com>
2022-08-09 20:55:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+//  HERO-BANNER STARTS
+$('.hero-link a').fancybox({
+    caption : function( instance, item ) {}
+  });
+// HERO-BANNER ENDS
+
+
+// COUNTER STARTS
+var a = 0;
+$(window).scroll(function () {
+    var oTop = $(".multi-card").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $(".counter").each(function () {
+            var $this = $(this),
+                countTo = $this.attr("data-number");
+            $({
+                countNum: $this.text()
+            }).animate(
+                {
+                    countNum: countTo
+                },
+
+                {
+                    duration: 850,
+                    easing: "swing",
+                    step: function () {
+                        //$this.text(Math.ceil(this.countNum));
+                        $this.text(
+                            Math.ceil(this.countNum).toLocaleString("en")
+                        );
+                    },
+                    complete: function () {
+                        $this.text(
+                            Math.ceil(this.countNum).toLocaleString("en")
+                        );
+                        //alert('finished');
+                    }
+                }
+            );
+        });
+        a = 1;
+    }
+});
+
+// COUNTER ENDS
